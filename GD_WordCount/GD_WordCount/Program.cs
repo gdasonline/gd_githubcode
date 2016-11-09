16e91e9cb7663b93913fc136c51c9810dc33e20a
gdasonline <gdasonline@gmail.com>
2016-11-09 13:23:04 +0000

    Girish Word Count
    Girish Word Count

---
+++ GD_WordCount/GD_WordCount/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GD_WordCount
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Accept the User Input
+            Console.Write("Input:");
+            string sentence = Console.ReadLine().ToLower();
+
+            //Split Sentence into words
+            string[] words = GetWordCollections(sentence);
+            
+            // Get Word Dictionary with their number count
+            var WordCountDict = GetWordCountNumbers(words);
+
+            //Write the Final Output
+            Console.WriteLine("Output:");
+            foreach (var word  in WordCountDict)
+            {
+                Console.WriteLine(word.Key + " "+  word.Value.ToString());
+
+            }
+
+            
+        }
+
+        public static string[] GetWordCollections ( string sentence)
+        {
+
+
+            //transform the sentence into array of  words 
+            string[] source = sentence.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '"' }, StringSplitOptions.RemoveEmptyEntries);
+            return source;
+
+        }
+
+
+        public static Dictionary<string,int> GetWordCountNumbers(string[] words)
+        {
+
+            // Calculate Word Count by their each group
+            var WordGroupCounts = from word in words
+                                  group word by word.ToLower()
+                                         into wordcountdata
+                                  select new { Word = wordcountdata.Key, Count = wordcountdata.Count() };
+
+            //Make Word count dictionary
+            var WordCountDict = WordGroupCounts.ToDictionary(wc => wc.Word, wc => wc.Count);
+            return WordCountDict;
+
+        }
+
+    }
+}
---
+++ GD_WordCount/UnitTestWordCount/UnitTestWordCount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GD_WordCount;
+
+namespace UnitTestWordCount
+{
+    [TestClass]
+    public class UnitTestWordCount
+    {
+        [TestMethod]
+        public void CanTestSplitsToWords()
+        {
+            Console.WriteLine("Testing to split into words");
+            string input = "This is a statement, and so is this.";
+            string[] expectedwordsplit = { "This", "is", "a", "statement", "and", "so", "is", "this" };
+            string[] actualwordsplit = Program.GetWordCollections(input);
+            CollectionAssert.AreEqual(expectedwordsplit,actualwordsplit);
+
+        }
+
+        [TestMethod]
+        public void CanTestWordCount()
+        {
+            Console.WriteLine("Testing count words");
+            string input = "This is a statement, and so is this.";
+            Dictionary<string, int> expctedcount = new Dictionary<string, int>
+            {
+                { "this", 2 },
+                { "is", 2 },
+                { "a", 1 },
+                { "statement", 1},
+                { "and", 1 },
+                { "so", 1 }
+
+            };
+
+            string[] actualwordsplit = Program.GetWordCollections(input);
+            Dictionary<string, int> actualcount = Program.GetWordCountNumbers(actualwordsplit);
+
+            CollectionAssert.AreEqual(expctedcount, actualcount);
+
+        }
+    }
+}
